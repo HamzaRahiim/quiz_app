ba2d7495489bd38dd3f0efe7440afffdcb5d167d
Hamza Rahim <hamzarahim836@gmail.com>
2024-09-01 16:54:37 +0500

    replace_icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Quiz Test Website",
   description: "Created By Hamza Rahim",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
 };
 
 export default function RootLayout({
